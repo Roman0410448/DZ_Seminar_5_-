79b0b083f3f9f702a44ac5ab00f1de9307b2ad4f
ROMAN <junk0410448@mail.ru>
2023-03-26 23:55:30 +0700

    Z_2

---
+++ Z_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] resultArray = GetArray(23, -1000, 1000);
+Console.WriteLine($"Массив: [ {String.Join("; ", resultArray)} ]");
+    
+    int[] GetArray (int size, int min, int max)
+     {
+         int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+         {
+             array[i] = new Random().Next(min, max + 1);
+         }
+        return array;
+    } 
+    int sum = 0;
+
+for (int z = 0; z < resultArray.Length; z+=2)
+    sum = sum + resultArray[z];
+
+    Console.WriteLine($"Всего {resultArray.Length} чисел, сумма элементов cтоящих на нечётных позициях = {sum}");        
