29f5386044d28539b8a57d3bca869b4d86d168f4
ROMAN <junk0410448@mail.ru>
2023-03-27 01:21:27 +0700

    Z_3

---
+++ Z_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+int Prompt (string message)
+ {
+     System.Console.Write(message);
+     int result = Convert.ToInt32(Console.ReadLine());
+     return result;
+ }
+ int size = Prompt("Введите длинну массива: ");
+double[] numbers = new double[size];
+double[] resultArray = GetArray(numbers);
+Console.WriteLine($"Массив: [ {String.Join("; ", resultArray)} ]");
+    
+    double[] GetArray (double[] numbers)
+     {
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().NextDouble())*100 ;
+        }
+        return numbers;
+    } 
+    double max = numbers[0]; 
+    double min = numbers[0]; 
+    for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > max)
+        {
+            max = numbers[z];
+        }
+    if (numbers[z] < min)
+        {
+            min = numbers[z];
+        }
+}
+double diff = max - min; 
+Console.WriteLine($"Длинна мвссива {numbers.Length}. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {diff}");
